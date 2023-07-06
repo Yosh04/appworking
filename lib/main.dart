dcee796f83f70eb6d5d367e676c5a13ec5273a60
Yosh <antonyescor4@gmail.com>
2023-07-06 10:42:08 -0600

    Finished the componente view list and working on navegation bar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
@@ -8,10 +7,9 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:untitled/generador.dart';
 import 'package:permission_handler/permission_handler.dart';
-
+import 'dart:async';
 
 //Paquetes de starlin.
-
 
 import 'package:open_file/open_file.dart'; // Importa la biblioteca open_file
 
@@ -42,6 +40,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _pickDirectory();
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,96 +62,155 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            ElevatedButton(onPressed: _pickDirectory, child: Text('LIST DIRECTORY FILES')),
-            SizedBox(height: 10),
-            ElevatedButton(onPressed: _pickDirectoryExter, child: Text('CREATE PFD')),
-            SizedBox(height: 10),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: files.length,
-              itemBuilder: (context, index) {
-                String fileName = basename(files[index].path);
-                return ElevatedButton(
-                  onPressed: () {
-                    _pickFile(files[index].path);
-                  },
-                  child: Text(fileName),
-                );
-              },
+            Container(
+              height: 60,
+              color: Colors.grey,
+              child: const Center(
+                child: Text(
+                  'Documentos Recientes',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: _createFile, child: const Text('CREATE PFD')),
+            const SizedBox(height: 10),
+            Container(
+              height: 550, // Ajusta la altura según tus preferencias
+              child: ListView.separated(
+                physics:
+                    const ClampingScrollPhysics(), // Habilita el desplazamiento vertical en el ListView
+                shrinkWrap: true,
+                itemCount: files.length,
+                itemBuilder: (context, index) {
+                  String fileName = basename(files[index].path);
+                  return ElevatedButton(
+                    onPressed: () {
+                      _pickFile(files[index].path);
+                    },
+                    child: Container(
+                      height: 60,
+                      child: Row(
+                        children: [
+                          const Icon(Icons.picture_as_pdf,
+                              color: Colors.red, size: 50),
+                          Flexible(
+                            child: Text(
+                              fileName,
+                              style: const TextStyle(fontSize: 16),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: 10),
+              ),
             ),
           ],
         ),
       ),
-
+      bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          height: 100,
+          color: Colors.brown,
+          child: Container(
+              child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                IconButton(
+                    onPressed: null,
+                    icon: Icon(
+                      Icons.add_circle_outline_outlined,
+                      color: Color(0xFF1C207F),
+                      size: 38,
+                    )),
+                IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.upload_file_outlined,
+                    color: Color(0xFF1C207F),
+                    size: 38,
+                  ),
+                ),
+                IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.settings,
+                    color: Color(0xFF1C207F),
+                    size: 38,
+                  ),
+                ),
+                IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.exit_to_app,
+                    color: Color(0xFF1C207F),
+                    size: 38,
+                  ),
+                )
+              ]
+              )
+          )
+      ),
     );
   }
-
-
-
 
   void _pickFile(String filePath) async {
     OpenFile.open(filePath); // Abre el archivo usando la ruta proporcionada
   }
 
   //final String _filePath = '/data/user/0/com.example.untitled/app_flutter/';
-  List<FileSystemEntity> files =[];
+  List<FileSystemEntity> files = [];
   Directory appDocumentsDirectory = Directory('/storage/emulated/0/books');
 
-  void _pickDirectoryExter() async {
-
-
-
-
-    var statusWR = await Permission.manageExternalStorage.request();
+  void _createFile() async {
+    var statusPermission = await Permission.manageExternalStorage.request();
 
     print(appDocumentsDirectory);
-
-    if (statusWR.isGranted) {
-      print(statusWR);
-      //listarArchivosRecursivos(appDocumentsDirectory);
+    if (statusPermission.isGranted) {
+      print(statusPermission);
       generatePDFAndOpen(appDocumentsDirectory);
+      timer = Timer.periodic(const Duration(seconds: 5), (Timer t) {
+        listarArchivosRecursivos(appDocumentsDirectory);
+        timer?.cancel();
+      });
     } else {
-      print(statusWR);
-    }
-
-
-  }
-
-  Future<void> requestStoragePermission() async {
-    var status = await Permission.storage.request();
-    if (status.isGranted) {
-
-    } else {
-
+      print(statusPermission);
     }
   }
 
   void _pickDirectory() async {
-    //Directory appDocumentsDirectory = Directory('/storage/emulated/0/books');
+    var statusPermission = await Permission.manageExternalStorage.request();
     Directory appDocumentsDirectory = Directory('/storage/emulated/0/books');
-    await requestStoragePermission(); // Esperar la ejecución de requestStoragePermission()
-
-
-    var statusWR = await Permission.manageExternalStorage.request();
-
-    print(appDocumentsDirectory);
-
-    if (statusWR.isGranted) {
-      print(statusWR);
+    if (statusPermission.isGranted) {
+      print('Permitido amigo mio.');
+      print(statusPermission);
       listarArchivosRecursivos(appDocumentsDirectory);
-      generatePDFAndOpen(appDocumentsDirectory);
+      timer = Timer.periodic(const Duration(seconds: 20), (Timer t) {
+        listarArchivosRecursivos(appDocumentsDirectory);
+      });
     } else {
-      print(statusWR);
+      print('Denegado amigo mio.');
+      print(statusPermission);
     }
-
   }
 
-
   Future<void> listarArchivosRecursivos(Directory directorio) async {
-    files = directorio.listSync(recursive: true).where((e) => e is File && e.path.endsWith('.pdf')).toList();
+    files = directorio
+        .listSync(recursive: true)
+        .where((e) => e is File && e.path.endsWith('.pdf'))
+        .toList();
     print(files);
     print(await directorio.exists());
     String absolutePath = directorio.absolute.path;
@@ -159,7 +229,4 @@ class _MyHomePageState extends State<MyHomePage> {
       });
     }
   }
-
-
-
 }
