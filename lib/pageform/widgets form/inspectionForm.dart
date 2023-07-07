3b6c2bad64f7d884b76e03cd89b4ce3cb82473fb
Yosh <antonyescor4@gmail.com>
2023-07-06 23:19:44 -0600

    Working on inpections

--- lib/pageform/widgets form/inspectionForm.dart
+++ lib/pageform/widgets form/inspectionForm.dart
@@ -1,117 +1,81 @@
 import 'package:flutter/material.dart';
 
 class InspectionForm extends StatefulWidget {
-  final ValueChanged<List<dynamic>> onFormValuesChanged;
-
-  InspectionForm({required this.onFormValuesChanged});
-
   @override
   _InspectionFormState createState() => _InspectionFormState();
 }
 
 class _InspectionFormState extends State<InspectionForm> {
-  TextEditingController _inspectionController = TextEditingController();
-  String _currentValue = '';
-
-  @override
-  void dispose() {
-    _inspectionController.dispose();
-    super.dispose();
-  }
+  DateTime currentDateSpecial = DateTime.now();
+  TimeOfDay? startTime;
+  TimeOfDay? endTime;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.green),
-            borderRadius: BorderRadius.circular(5.0),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text('Fecha:'),
-              SizedBox(width: 10.0),
-              ElevatedButton(
-                onPressed: () {
-                  _showDatePicker(context);
-                },
-                child: Text('Seleccionar fecha'),
-              ),
-            ],
-          ),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _buildFormField("Cantidad Aprox De Combustible"),
+            SizedBox(height: 16.0),
+            _buildTimePicker("Hora de inicio", startTime, (time) {
+              setState(() {
+                startTime = time;
+              });
+            }),
+            SizedBox(height: 16.0),
+            _buildTimePicker("Hora de final", endTime, (time) {
+              setState(() {
+                endTime = time;
+              });
+            }),
+          ],
         ),
-        Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.green),
-            borderRadius: BorderRadius.circular(5.0),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text('Hora:'),
-              SizedBox(width: 10.0),
-              ElevatedButton(
-                onPressed: () {
-                  _showTimePicker(context);
-                },
-                child: Text('Seleccionar hora'),
-              ),
-            ],
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.green),
-            borderRadius: BorderRadius.circular(5.0),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Numero de motores:'),
-              SizedBox(width: 10.0),
-              Expanded(
-                child: TextField(
-                  controller: _inspectionController,
-                  onChanged: (value) {
-                    setState(() {
-                      _currentValue = value;
-                    });
-                  },
-                  onEditingComplete: () {
-                    widget.onFormValuesChanged([_currentValue]);
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
+      ),
     );
   }
 
-  void _showDatePicker(BuildContext context) {
-    showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2100),
-    ).then((selectedDate) {
-      if (selectedDate != null) {
-        widget.onFormValuesChanged([selectedDate]);
-      }
-    });
+  Widget _buildFormField(String label) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(),
+        borderRadius: BorderRadius.circular(4.0),
+      ),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: label,
+          border: InputBorder.none,
+        ),
+      ),
+    );
   }
 
-  void _showTimePicker(BuildContext context) {
-    showTimePicker(
-      context: context,
-      initialTime: TimeOfDay.now(),
-    ).then((selectedTime) {
-      if (selectedTime != null) {
-        widget.onFormValuesChanged([selectedTime]);
-      }
-    });
+  Widget _buildTimePicker(
+      String label, TimeOfDay? selectedTime, Function(TimeOfDay) onTimeSelected) {
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(),
+          borderRadius: BorderRadius.circular(4.0),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: Text(
+                  label,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Text(selectedTime?.format(context) ?? ''),
+          ],
+        ),
+      ),
+    );
   }
 }
