373cc4e49856cc494707b92fae29b146389692c6
Yosh <antonyescor4@gmail.com>
2023-07-06 23:37:10 -0600

    Nuevos cambios

--- lib/pageform/widgets form/inspectionForm.dart
+++ lib/pageform/widgets form/inspectionForm.dart
@@ -19,13 +19,13 @@ class _InspectionFormState extends State<InspectionForm> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             _buildFormField("Cantidad Aprox De Combustible"),
-            SizedBox(height: 16.0),
+            SizedBox(height: 16),
             _buildTimePicker("Hora de inicio", startTime, (time) {
               setState(() {
                 startTime = time;
               });
             }),
-            SizedBox(height: 16.0),
+            SizedBox(height: 16),
             _buildTimePicker("Hora de final", endTime, (time) {
               setState(() {
                 endTime = time;
@@ -38,15 +38,31 @@ class _InspectionFormState extends State<InspectionForm> {
   }
 
   Widget _buildFormField(String label) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(),
-        borderRadius: BorderRadius.circular(4.0),
+    return Theme(
+      data: ThemeData(
+        primaryColor: const Color(0xFF1C207F), // Color del texto y el borde
+        hintColor: const Color(0xFF1C207F), // Color del texto de sugerencia
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: const Color(0xFF1C207F)), // Color del borde
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: const Color(0xFF1C207F)), // Color del borde cuando está enfocado
+          ),
+          filled: true, // Relleno activado
+          fillColor: Colors.white, // Color de fondo
+        ),
       ),
-      child: TextFormField(
-        decoration: InputDecoration(
-          labelText: label,
-          border: InputBorder.none,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(),
+          borderRadius: BorderRadius.circular(4.0),
+        ),
+        child: TextFormField(
+          decoration: InputDecoration(
+            labelText: label,
+            border: InputBorder.none,
+          ),
         ),
       ),
     );
@@ -54,28 +70,58 @@ class _InspectionFormState extends State<InspectionForm> {
 
   Widget _buildTimePicker(
       String label, TimeOfDay? selectedTime, Function(TimeOfDay) onTimeSelected) {
-    return SizedBox(
-      width: double.infinity,
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(),
-          borderRadius: BorderRadius.circular(4.0),
+    return Theme(
+      data: ThemeData(
+        primaryColor: const Color(0xFF1C207F), // Color del texto y el borde
+        hintColor: const Color(0xFF1C207F), // Color del texto de sugerencia
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: const Color(0xFF1C207F)), // Color del borde
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: const Color(0xFF1C207F)), // Color del borde cuando está enfocado
+          ),
+          filled: true, // Relleno activado
+          fillColor: Colors.white, // Color de fondo
         ),
-        child: Row(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                child: Text(
-                  label,
-                  style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      child: SizedBox(
+        width: double.infinity,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(),
+            borderRadius: BorderRadius.circular(4.0),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Text(
+                    label,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-            ),
-            Text(selectedTime?.format(context) ?? ''),
-          ],
+              ElevatedButton(
+                onPressed: () => _selectTime(selectedTime, onTimeSelected),
+                child: Text(selectedTime?.format(context) ?? 'Seleccionar'),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<void> _selectTime(
+      TimeOfDay? selectedTime, Function(TimeOfDay) onTimeSelected) async {
+    final TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: selectedTime ?? TimeOfDay.now(),
+    );
+    if (pickedTime != null && pickedTime != selectedTime) {
+      onTimeSelected(pickedTime);
+    }
   }
 }
